594a0c92507d1890603e1b05207f6ff276f02f3c
OlgaKostiuk <olga.d.kostiuk@gmail.com>
2018-01-11 22:41:02 +0200

    Add project files.

---
+++ BeautySalon/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeautySalon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contacts()
+        {
+            return View();
+        }
+
+        public ActionResult HaircutAndStyling()
+        {
+            return View();
+        }
+
+        public ActionResult HairColoring()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+    }
+}
